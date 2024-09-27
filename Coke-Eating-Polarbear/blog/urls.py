47854fdd98185d4dc08d6b4aa2aac45ac88aed97
unknown <audwns990625@gmail.com>
2024-09-27 18:42:48 +0900

    template merge

--- Coke-Eating-Polarbear/blog/urls.py
+++ Coke-Eating-Polarbear/blog/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from . import views
 
+
 urlpatterns = [
     # blog 앱 내부의 경로를 지정할 부분
     path('', views.main,name='main'), # 없는 경로를 호출하고 있음
-    path('portfolio-details/', views.service_details, name='portfolio-details'), 
-    path('service-details/', views.portfolio_details, name='service-details'), 
+    path('portfolio-details.html', views.portfolio_details, name='portfolio-details'), 
+    path('service-details.html', views.service_details, name='service-details'), 
 ]
